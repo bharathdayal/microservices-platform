f0460d9d66840c2a0c639943f5174b681e5d004d
bharathdayal <bharathdayals@gmail.com>
2025-12-31 21:05:04 +0530

    Update OutboxEventPublisher.java

--- order-service/src/main/java/com/example/order_service/service/OutboxEventPublisher.java
+++ order-service/src/main/java/com/example/order_service/service/OutboxEventPublisher.java
@@ -2,6 +2,7 @@ package com.example.order_service.service;
 
 import com.example.order_service.domain.OutboxStatus;
 import com.example.order_service.repo.OutboxRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -14,28 +15,35 @@ import org.springframework.stereotype.Component;
 public class OutboxEventPublisher {
 
     private final OutboxRepository outboxRepository;
-    private final KafkaTemplate<String,String> kafka;
+    private final KafkaTemplate<String,Object> kafka;
     private final String topic;
+    private final ObjectMapper objectMapper;
 
-    public OutboxEventPublisher(OutboxRepository outboxRepository, KafkaTemplate<String, String> kafka, @Value("${order.topic.name}") String topic) {
+    public OutboxEventPublisher(OutboxRepository outboxRepository, KafkaTemplate<String, Object> kafka, @Value("${order.topic.name}") String topic,ObjectMapper objectMapper) {
         this.outboxRepository = outboxRepository;
         this.kafka = kafka;
         this.topic = topic;
+        this.objectMapper=objectMapper;
     }
 
     @Scheduled(fixedDelay = 3000)
     @Transactional
     public void publish() {
 
-        var events=outboxRepository.findByStatus(OutboxStatus.PENDING);
+        var events = outboxRepository.findByStatus(OutboxStatus.PENDING);
 
-        for(var e:events) {
+        for (var e : events) {
             try {
+                Object event =
+                        objectMapper.readValue(e.getPayload(), Object.class);
+
                 kafka.send(
                         topic,
                         e.getAggregateId().toString(),
-                        e.getPayload()).get();
-                        e.setStatus(OutboxStatus.SENT);
+                        event
+                ).get();
+
+                e.setStatus(OutboxStatus.SENT);
 
             } catch (Exception ex) {
                 e.setStatus(OutboxStatus.FAILED);
